88c74ba99a62b34e9da174b3070c519edfcce4b5
Javier Gomez <javier.gov@outlook.es>
2024-08-11 01:09:18 -0600

    Evento resize

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,14 +1,29 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     let button = document.querySelector(".layout__menu-toggle")
     let aside = document.querySelector(".layout__aside")
+    let icon__bars = document.querySelector(".layout__menu-toggle .fa-bars")
+    let icon__xmark = document.querySelector(".layout__menu-toggle .fa-xmark")
 
     button.addEventListener("click", (event) => {
         let visible = document.querySelector(".layout__aside--visible")
         if(!visible){
             aside.classList.add("layout__aside--visible")
+            icon__bars.style.opacity = 0
+            icon__xmark.style.opacity = 1
         } else{
             aside.classList.remove("layout__aside--visible")
-
+            icon__bars.style.opacity = 1
+            icon__xmark.style.opacity = 0
         }
     })
+
+    window.addEventListener("resize", (event) => {
+        let size = parseInt(document.body.clientWidth);
+        if(size <= 1060){
+            aside.classList.remove("layout__aside--visible")
+            icon__bars.style.opacity = 1
+            icon__xmark.style.opacity = 0
+        }
+    })
+
 })
